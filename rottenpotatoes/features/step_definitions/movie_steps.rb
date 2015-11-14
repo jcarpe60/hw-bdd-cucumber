a48f9b90417a4ba0e547d3d646ba0222c3d12a53
Jennings Carpenter <Jennings.Carpenter@gmail.com>
2015-11-14 23:20:36 +0000

    hw 3 part 2 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,8 +3,6 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
-    movie = Hash.new()
-    
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
   end
@@ -16,8 +14,11 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  #match = /#{e1}.*#{e2}/m =~ page.body
-
+  #assert page.body.index(e1) < page.body.index(e2)
+  #page.body.should match =~ /#{e1}.*#{e2}/m
+  assert_match(/#{e1}.*#{e2}/m, page.body)
+  #assert page.body =~ /#{e1}.+#{e2}/m
+  fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -28,12 +29,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(/,\s*/).each do |rating|
+    uncheck ? uncheck("ratings_#{rating}") : check("ratings_#{rating}")
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  Movie.all
-  fail "Unimplemented"
+  page.all('table#movies tr').count.should equal Movie.all.length + 1
+  #fail Unimplemented
 end
 
